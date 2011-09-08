a061f95c6a67ab502268fe8326db2de8ca8cb422
Andery <fxklim@gmail.com>
2011-09-08 13:09:28 +0600

    Реализовано переключение слайдов при переходе по ссылкам в меню

--- js/slider.scripts.js
+++ js/slider.scripts.js
@@ -1,10 +1,37 @@
+        
+            
+            function getStart(){
+                
+                var str = document.location.href;
+                var p = str.indexOf('?');
+                if (str.indexOf('?') + 1){
+                    parts=String(document.location).split("?",2)[1].split("&");
+                    GET={};
+                    for (i=0; i<parts.length; i++) {
+                       curr = parts[i].split('=');
+                       GET[curr[0]] = curr[1];
+                    }
+                    return GET.slide;
+                }
+                else
+                {
+                    return 1;
+                }
+                
+                return 1;
+
+            }        
+        
         $(function(){
+            
+            
             $('#slides').slides({
                 preload: true,
                 preloadImage: 'img/loading.gif',
                 play: 5000,
                 pause: 2500,
-                hoverPause: true
+                hoverPause: true,
+                start: getStart()
             });
             
 
