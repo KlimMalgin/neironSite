2f4414d3d803d65f0175c8d1dde7ebd299a514ab
Andery <fxklim@gmail.com>
2011-09-12 17:24:34 +0600

    Добавлена кнопка Go напротив заголовка

--- js/slider.scripts.js
+++ js/slider.scripts.js
@@ -1,30 +1,31 @@
         
-            
-            function getStart(){
-                
-                var str = document.location.href;
-                var p = str.indexOf('?');
-                if (str.indexOf('?') + 1){
-                    parts=String(document.location).split("?",2)[1].split("&");
-                    GET={};
-                    for (i=0; i<parts.length; i++) {
-                       curr = parts[i].split('=');
-                       GET[curr[0]] = curr[1];
-                    }
-                    return GET.slide;
-                }
-                else
-                {
-                    return 1;
-                }
-                
-                return 1;
+        // Возвращает номер слайда с которого следует начать показ
+        function getStart(){
 
-            }        
+            var str = document.location.href;
+            var p = str.indexOf('?');
+            if (str.indexOf('?') + 1){
+                parts=String(document.location).split("?",2)[1].split("&");
+                GET={};
+                for (i=0; i<parts.length; i++) {
+                   curr = parts[i].split('=');
+                   GET[curr[0]] = curr[1];
+                }
+                return GET.slide;
+            }
+            else
+            {
+                return 1;
+            }
+
+            return 1;
+
+        }
+        
         
         $(function(){
             
-            
+            // Настройка слайдера
             $('#slides').slides({
                 preload: true,
                 preloadImage: 'img/loading.gif',
@@ -34,7 +35,7 @@
                 start: getStart()
             });
             
-
+            // Необходимо для выравнивания кнопок-навигации по слайдам
             var _width = 0;
             $("ul.pagination li").each(
                 function(){_width += 35;}
